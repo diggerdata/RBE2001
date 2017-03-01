8bd18092550e90b2288a9bae74b17d64325c70c9
Ryano647 <ryano647@gmail.com>
2017-02-28 22:07:46 -0500

    Switch Case Framework 1.4
    first three cases, purely logical

--- src/main.cpp
+++ src/main.cpp
@@ -52,7 +52,7 @@ void auton () { // auton by task number. Everything passed the commented out blo
     chassis.drive(0, 0);
     switch (state) {
       case kExtractRod:
-      if(true /*robot is on a line*/) {
+      if(true /*robot is on a line*/) { //STILL NEEDS TO LINE FOLLOW WHILE DRIVING
         while(true/*limit switch is not pressed*/)  //WILL need some kind of fail safe, could drive forever rn
             chassis.drive(mtrFwd);  //drive forward until limit switch is pressed
         chassis.stop(); //may need some delay before stopping
@@ -63,21 +63,75 @@ void auton () { // auton by task number. Everything passed the commented out blo
         //some delay
         arm.setHigh();
         //some delay
-        
-
+        chassis.drive(mtrRwd);
+        //calculated delay to prepare to turn around
+        chassis.stop();
+        //turn around here. needs line sensor and/or encoders. stop once turned then break.
       }
 
       break;
       case kStoreRod:
+      if((msg.getStorageAvl() && BIT0) == BIT0) { //SHOULD check if closest tube is open. Might be backwards
+        //drive until first line, then turn.
+        //get rod, then turn around and drive until line.
+        //drive back to reactor and turn around, like above
+      }
+      else if((msg.getStorageAvl() && BIT1) == BIT1) {  //checks if next tube is open
+        //drive until second line
+        //get rod, then turn around and drive until line.
+        //drive back to reactor and turn around, like above
+      }
+      else if(((msg.getStorageAvl() && BIT2) == BIT2)) {  //etc
+        //drive until third line
+        //get rod, then turn around and drive until line
+        //drive back to reactor and turn around, like above
+      }
+      else if((msg.getStorageAvl() && BIT3) == BIT3) {  //checks if last tube is open
+        //drive until fourth line
+        //get rod, then turn around and drive until line
+        //drive back to reactor and turn around, like above
+      }
+      else {
+        chassis.stop();
+        Serial.println("ERROR: Bluetooth message incorrect");
+        //LED flashing for visual indication
+      }
+
 
       break;
       case kGetNewRod:
-
+      if((msg.getSupplyAvl() && BIT0) == BIT0) { //SHOULD check if closest tube is open. Might be backwards
+        //drive until first line, then turn.
+        //get rod, then turn around and drive until line
+        //drive back to reactor and turn around, like above
+      }
+      else if((msg.getSupplyAvl() && BIT1) == BIT1) {  //checks if next tube is open
+        //drive until second line
+        //get rod, then turn around and drive until line
+        //drive back to reactor and turn around, like above
+      }
+      else if(((msg.getSupplyAvl() && BIT2) == BIT2)) {  //etc
+        //drive until third line
+        //get rod, then turn around and drive until line
+        //drive back to reactor and turn around, like above
+      }
+      else if((msg.getSupplyAvl() && BIT3) == BIT3) {  //checks if last tube is open
+        //drive until fourth line
+        //get rod, then turn around and drive until line
+        //drive back to reactor and turn around, like above
+      }
+      else {
+        chassis.stop();
+        Serial.println("ERROR: Bluetooth message incorrect");
+        //LED flashing for visual indication
+      }
       break;
       case kReplaceRod:
 
       break;
       default:
+      Serial.print("ERROR");
+      //make LED flash for visual indication
 
       break;
     }
@@ -105,7 +159,7 @@ signed char turn () { //code to turn. 0= line detected 1= no line
 
   if ((sensorValue | 0b00000000) == 0b11111111) {
       chassis.drive(0, 0);
-      tubeLinesCrossed++;
+      tubeLinesCrossed++; //may be the wrong place for this, dont want it to double count lines
   }
 
   if ((sensorValue | 0b00011000) == 0b11111111) {
