3bfb657b15dc89a3932ca0f760c797bf8342c280
Ryano647 <ryano647@gmail.com>
2017-02-28 20:15:33 -0500

    Switch Case Framework 1.2

--- src/main.cpp
+++ src/main.cpp
@@ -53,13 +53,12 @@ void auton () { // auton by task number. Everything passed the commented out blo
     chassis.drive(0, 0);
     switch (state) {
       case kExtractRod:
-
       break;
       case kStoreRod:
 
       break;
       case kGetNewRod:
-      
+
       break;
       case kReplaceRod:
 
@@ -82,7 +81,7 @@ void update () { // update method. Call this to excecute stored states
 
 }
 
-signed char turn () { //code to turn
+signed char turn () { //code to turn. 0= line detected 1= no line
   char sensorValue = linesensor.getArray();
 
   if ((sensorValue | 0b00000000) == 0b11111111) {
