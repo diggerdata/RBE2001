6d88d3776a8939e69905f99cbf528a3b3be399fa
jvahe <jvahe@live.com>
2017-02-28 20:43:03 -0500

    commented out encoder code

--- src/EncoderRight.c
+++ src/EncoderRight.c
@@ -1,11 +1,10 @@
+#ifndef ENCR_H_
+#define ENCR_H_
 /*
 *adapted from http://yameb.blogspot.com/2012/11/quadrature-encoders-in-arduino-done.html
 *Quadrature Decoder
 */
 #include "Arduino.h"
-#include <digitalWriteFast.h>  // library for high performance reads and writes by jrraines
-                               // see http://www.arduino.cc/cgi-bin/yabb2/YaBB.pl?num=1267553811/0
-                               // and http://code.google.com/p/digitalwritefast/
 
 // It turns out that the regular digitalRead() calls are too slow and bring the arduino down when
 // I use them in the interrupt routines while the motor runs at full speed.
@@ -42,6 +41,7 @@ int ParseEncoderRight(){
     if(_RightEncoderASet && _RightEncoderBSet) return 1;
     if(!_RightEncoderASet && !_RightEncoderBSet) return -1;
   }
+  return 0;
 }
 
 long getEncTicksRight()
@@ -52,8 +52,8 @@ long getEncTicksRight()
 
 // Interrupt service routines for the Right motor's quadrature encoder
 void HandleRightMotorInterruptA(){
-  _RightEncoderBSet = digitalReadFast(c_RightEncoderPinB);
-  _RightEncoderASet = digitalReadFast(c_RightEncoderPinA);
+  _RightEncoderBSet = digitalRead(c_RightEncoderPinB);
+  _RightEncoderASet = digitalRead(c_RightEncoderPinA);
 
   _RightEncoderTicks+=ParseEncoderRight();
 
@@ -64,11 +64,14 @@ void HandleRightMotorInterruptA(){
 // Interrupt service routines for the right motor's quadrature encoder
 void HandleRightMotorInterruptB(){
   // Test transition;
-  _RightEncoderBSet = digitalReadFast(c_RightEncoderPinB);
-  _RightEncoderASet = digitalReadFast(c_RightEncoderPinA);
+  _RightEncoderBSet = digitalRead(c_RightEncoderPinB);
+  _RightEncoderASet = digitalRead(c_RightEncoderPinA);
 
   _RightEncoderTicks+=ParseEncoderRight();
 
   _RightEncoderAPrev = _RightEncoderASet;
   _RightEncoderBPrev = _RightEncoderBSet;
 }
+
+
+#endif
